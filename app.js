19af4a403818fc4637f6ea09aa2c7b499f3752bc
John R. Estes <John.R.Estes@icloud.com>
2023-12-26 16:47:42 -0500

    small text change

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     // set response content    
         res.send(`<html>
                     <body>
-                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: This is AMAZING!!! Like & Subscribe!</h1>
+                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: This is AMAZING!!! JRE</h1>
                         <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
                             <img src="https://picsum.photos/400/400?random=1">
                         </div>

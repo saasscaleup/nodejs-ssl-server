72ba9f2fe8bb40c6df257ba39b5debf8f46489fe
John R. Estes <John.R.Estes@icloud.com>
2024-01-22 20:10:07 -0500

    Post code change

--- app.js
+++ app.js
@@ -43,17 +43,15 @@ app.get('/api/victron/data', async (req, res) => {
 
 app.post('/api/generator/data', async (req, res) => {
     try {
-        // Ensure req.body is defined and has a 'status' property
         const { status } = req.body || {};
 
         if (status !== undefined) {
             // Your logic to handle the status and update the global variable
-            // For example, you can store the status in a database, etc.
             console.log("Received status:", status);
             globalGeneratorStatus = status;
             res.json({ message: "Status received and stored successfully." });
         } else {
-            // Invalid or missing 'status' property in the request body
+            // Send a more generic error response for any missing or invalid payload
             res.status(400).json({ error: 'Invalid or missing status in the request body.' });
         }
     } catch (error) {
@@ -61,6 +59,7 @@ app.post('/api/generator/data', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
 
 // New route for the GET request to retrieve the stored status
 app.get('/api/generator/status', (req, res) => {
